cb47bc39c603ec6d7bec2654873a44cae88e2861
themagiche <magiche.mc@gmail.com>
2019-07-01 11:05:40 +0300

    Added frontend to public folder

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 3000;
 
 /* Global Config */
 app.disable("etag").disable("x-powered-by");
-app.use(express.static(path.join(__dirname, "public"))); // Set up public folder
 app.use(express.json());
 app.use(
     express.urlencoded({
@@ -24,6 +23,26 @@ mongoose.connect("mongodb://localhost:27017/fortHotel").then(
         console.log('Error when connecting to the database' + err)
     }
 );
+// ROutes
+
+
+//Handle production
+if (process.env.NODE_ENV === 'production') {
+    // Statiic folder
+    app.use(express.static(__dirname + '/public/')); // Set up public folder
+    // Handle SPA
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, "public/index.html"));
+    });
+};
+
+
+// Check on dev mode don't uncomment
+// app.use(express.static(__dirname + '/public/')); // Set up public folder
+// // Handle SPA
+// app.get(/.*/, (req, res) => {
+//     res.sendFile(__dirname + "/public/index.html");
+// });
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
