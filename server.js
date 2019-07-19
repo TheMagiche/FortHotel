ac0dcb580487537bec49bc22f58b8b095064038e
themagiche <magiche.mc@gmail.com>
2019-07-19 13:09:38 +0300

    ui gallery and mail

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const nodeMailer = require("nodemailer");
+// const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 
 /* Global Config */
@@ -14,21 +15,82 @@ app.use(
   })
 );
 app.use(cors());
+
 // DATABASE
-mongoose
-  .connect("mongodb://localhost:27017/fortHotel", {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  })
-  .then(
-    () => {
-      console.log("Database connection is successful");
-    },
-    err => {
-      console.log("Error when connecting to the database" + err);
+// mongoose
+//   .connect("mongodb://localhost:27017/fortHotel", {
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+//   })
+//   .then(
+//     () => {
+//       console.log("Database connection is successful");
+//     },
+//     err => {
+//       console.log("Error when connecting to the database" + err);
+//     }
+//   );
+
+// Routes
+app.post("/send-email", function(req, res) {
+  let transporter = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      // should be replaced with real sender's account
+      user: "krammax@gmail.com",
+      pass: 235209006
     }
-  );
-// ROutes
+  });
+  let mailOptions = {
+    // should be replaced with real recipient's account
+    to: "lullabyltd@hotmail.com",
+    subject: "Contact Form from:  " + req.body.subject,
+    body: "Message:  " + req.body.message,
+    html: `<h3> ${req.body.message} </h3>`
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message %s sent: %s", info.messageId, info.response);
+  });
+  res.writeHead(301, {
+    Location: "public/index.html"
+  });
+  res.end();
+});
+
+app.post("/form", function(req, res) {
+  let transporter = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      // should be replaced with real sender's account
+      user: "krammax@gmail.com",
+      pass: 235209006
+    }
+  });
+  let mailOptions = {
+    // should be replaced with real recipient's account
+    to: "lullabyltd@hotmail.com",
+    subject: "Contact Form from:  " + req.body.subject,
+    body: "Message:  " + req.body.message,
+    html: `<h3> ${req.body.message} </h3>`
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message %s sent: %s", info.messageId, info.response);
+  });
+  res.writeHead(301, {
+    Location: "public/index.html"
+  });
+  res.end();
+});
 
 //Handle production
 if (process.env.NODE_ENV === "production") {
